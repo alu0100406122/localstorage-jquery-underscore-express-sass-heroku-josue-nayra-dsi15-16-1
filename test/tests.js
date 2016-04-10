7543d18c9a46a0d22c33dd03a910ccfc36b71ecf
alu0100406122 <alu0100406122@ull.edu.es>
2016-04-10 11:01:56 +0000

    Probando nuevas pruebas.

--- test/tests.js
+++ test/tests.js
@@ -5,20 +5,33 @@ describe('Comma separated values',function()
 {
     describe('Function calculate',function()
     {
-        it("Se genera una tabla de 4 filas",function()
-        {
-            /*var auxiliar = calculate("\"producto\",\"precio\"\n\"caca\",\"vaca\"\n\"producto\",\"precio\"\n\"caca\",\"vaca\"");
-            $.each(aux,function(campo,valor)
-            {
-                console.log("Campo:"+campo+", Valor:" +valor);
-                $.each(valor,function(campo1,valor1)
-                {
-                    console.log("Campo1: "+campo1+",valor1: "+valor1); 
-                });
-            });
-            console.log("Longitud del tema:" + aux.length);*/
-            expect(calculate("\"Animal\",\"Patas\"\n\"Perro\",\"4\"\n\"Gallina\",\"2\"\n\"Araña\",\"8\"").length).to.be.equal(4);    
-        }); 
+      it("Se genera una tabla de 4 filas",function()
+      {
+          /*var auxiliar = calculate("\"producto\",\"precio\"\n\"caca\",\"vaca\"\n\"producto\",\"precio\"\n\"caca\",\"vaca\"");
+          $.each(aux,function(campo,valor)
+          {
+              console.log("Campo:"+campo+", Valor:" +valor);
+              $.each(valor,function(campo1,valor1)
+              {
+                  console.log("Campo1: "+campo1+",valor1: "+valor1); 
+              });
+          });
+          console.log("Longitud del tema:" + aux.length);*/
+          expect(calculate("\"Animal\",\"Patas\"\n\"Perro\",\"4\"\n\"Gallina\",\"2\"\n\"Araña\",\"8\"").length).to.be.equal(4);    
+      });
+      
+      it ('Comprobando que cada fila de la tabla generada tiene los valores correctos...',function(){
+        this.original = "\"Animal\",\"Patas\"\n\"Perro\",\"4\"\n\"Gallina\",\"2\"";
+        var auxiliar = calculate(this.original);
+        expect(auxiliar[0].value).to.deep.equal(['Animal','Patas']);
+        expect(auxiliar[1].value).to.deep.equal(['Perro', '4']);
+        expect(auxiliar[2].value).to.deep.equal(['Gallina', '2']);
+      })
+        
+    });
+    
+    describe('Expresión regular',function(){
+      
     });
     
 
